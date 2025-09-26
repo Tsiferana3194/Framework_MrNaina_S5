e707db32cf63ec69cc33a464b2e52af8f04fc658
ETU003143 <fanomezantsoarandriatsiferana@gmail.com>
2025-09-26 22:57:50 +0300

    dossier framework
    mise en place elements du dossier framework

--- Sprint1/framework/src/main/java/com/framework/core/FrontServlet.java
+++ Sprint1/framework/src/main/java/com/framework/core/FrontServlet.java
@@ -1,3 +1,21 @@
-public class FrontServlet {
-    
+package com.framework.core;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import java.io.IOException;
+
+public class FrontServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/plain");
+        response.getWriter().println("FrontServlet is working! URL pattern: /");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        doGet(request, response);
+    }
 }
